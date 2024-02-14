72ad50fd6d59f4b99c58aad1e988e06eca2c4036
Francisco Wallison <franciscowallison@gmail.com>
2024-02-14 10:14:22 -0300

    add logica de serviceWorker

---
+++ service-worker.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = "v0";
+const urlsToCache = ["/", "/index.html"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return fetch(event.request).then((response) => {
+        cache.put(event.request, response.clone());
+      });
+    })
+  );
+});
